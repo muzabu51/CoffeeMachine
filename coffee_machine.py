e5260a559aa2ab983b8e8b317473fd2d09bebbc1
muzabu51 <muzabu51@gmail.com>
2020-06-14 11:47:04 +0530

    Coffee machine simulation using classes

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,163 +1,105 @@
-# Cappuccino
-Cap_reqd_water = 200  # ml
-Cap_reqd_milk = 100  # ml
-Cap_reqd_beans = 12  # g
-Cap_cost = 6  # $
-# Espresso
-Esp_reqd_water = 250  # ml
-Esp_reqd_milk = 0  # ml
-Esp_reqd_beans = 16  # g
-Esp_cost = 4  # $
-# Latte
-Lat_reqd_water = 350  # ml
-Lat_reqd_milk = 75  # ml
-Lat_reqd_beans = 20  # g
-Lat_cost = 7  # $
-# available quantities
-qty_water = 400
-qty_milk = 540
-qty_beans = 120
-no_of_cups = 9
-money = 550
+class CoffeeMachine:
+    # Cappuccino
+    cappuccino = {'water': 200, 'milk': 100, 'beans': 12, 'cost': 6}
 
+    # Espresso
+    espresso = {'water': 250, 'milk': 0, 'beans': 16, 'cost': 4}
 
-def fill():
-    global qty_water
-    global qty_milk
-    global qty_beans
-    global no_of_cups
-    global money
-    qty_water += int(input('Write how many ml of water do you want to add: '))
-    qty_milk += int(input('Write how many ml of milk do you want to add: '))
-    qty_beans += int(input('Write how many grams of coffee beans do you want to add: '))
-    no_of_cups += int(input('Write how many ml disposable cups of coffee do you want to add: '))
-    print()
+    # Latte
+    latte = {'water': 350, 'milk': 75, 'beans': 20, 'cost': 7}
 
+    def __init__(self, water, milk, beans, cups, money, state='main'):
+        self.water = water
+        self.milk = milk
+        self.beans = beans
+        self.cups = cups
+        self.money = money
+        self.state = state
+        self.possibility = 1
 
-def take():
-    global money
-    print(f'I gave you ${money}')
-    money = 0
-    print()
+    def fill(self):
+        self.water += int(input('Write how many ml of water do you want to add:\n '))
+        self.milk += int(input('Write how many ml of milk do you want to add:\n '))
+        self.beans += int(input('Write how many grams of coffee beans do you want to add:\n '))
+        self.cups += int(input('Write how many ml disposable cups of coffee do you want to add:\n '))
+        self.possibility = 1
+        print()
 
+    def take(self):
+        print(f'I gave you ${self.money}')
+        self.money = 0
+        print()
 
-def output():
-    print('The coffee machine has:')
-    print(f'{qty_water} of water')
-    print(f'{qty_milk} of milk')
-    print(f'{qty_beans} of coffee beans')
-    print(f'{no_of_cups} of disposable cups')
-    print(f'${money} of money')
-    print()
+    def output(self):
+        print('The coffee machine has:')
+        print(f'{self.water} of water')
+        print(f'{self.milk} of milk')
+        print(f'{self.beans} of coffee beans')
+        print(f'{self.cups} of disposable cups')
+        print(f'${self.money} of money')
+        print()
 
+    def msg(self, txt):
+        print(f'Sorry, not enough {txt}!')
 
-def msg(txt):
-    print(f'Sorry, not enough {txt}!')
+    def buycheck(self, coffee):
+        if self.water < coffee['water']:
+            self.msg('water')
+            self.possibility = 0
+        elif self.milk < coffee['milk']:
+            self.msg('milk')
+            self.possibility = 0
+        elif self.beans < coffee['beans']:
+            self.msg('coffee beans')
+            self.possibility = 0
+        elif self.cups == 0:
+            self.possibility = 0
+            self.msg('disposable cups')
 
-
-def buy():
-    inp = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: , back - to main menu: ')
-    global qty_water
-    global qty_milk
-    global qty_beans
-    global no_of_cups
-    global money
-    possibility = 1
-    if inp == '1':
-        if qty_water < Esp_reqd_water:
-            msg('water')
-            possibility = 0
-            print()
-        elif qty_milk < Esp_reqd_milk:
-            msg('milk')
-            possibility = 0
-            print()
-        elif qty_beans < Esp_reqd_beans:
-            msg('coffee beans')
-            possibility = 0
-            print()
-        elif no_of_cups == 0:
-            possibility = 0
-            msg('disposable cups')
-        if possibility:
-            qty_water -= Esp_reqd_water
-            qty_milk -= Esp_reqd_milk
-            qty_beans -= Esp_reqd_beans
-            no_of_cups -= 1
-            money += Esp_cost
+    def checkpos(self, coffee):
+        if self.possibility:
+            self.water -= coffee['water']
+            self.milk -= coffee['milk']
+            self.beans -= coffee['beans']
+            self.cups -= 1
+            self.money += coffee['cost']
             print('I have enough resources, making you a coffee!')
         print()
-    elif inp == '2':
-        if qty_water < Lat_reqd_water:
-            msg('water')
-            possibility = 0
+
+    def buy(self):
+        inp = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:\n ')
+        if inp == '1':
+            self.buycheck(self.espresso)
+            self.checkpos(self.espresso)
+        elif inp == '2':
+            self.buycheck(self.latte)
+            self.checkpos(self.latte)
+        elif inp == '3':
+            self.buycheck(self.cappuccino)
+            self.checkpos(self.cappuccino)
+        elif inp == 'back':
+            self.main()
+
+    def main(self):
+        inp = input('Write action (buy, fill, take, remaining, exit):\n ')
+        if inp == 'buy':
             print()
-        elif qty_milk < Lat_reqd_milk:
-            msg('milk')
-            possibility = 0
+            self.buy()
+        elif inp == 'fill':
             print()
-        elif qty_beans < Lat_reqd_beans:
-            msg('coffee beans')
-            possibility = 0
+            self.fill()
+        elif inp == 'take':
             print()
-        elif no_of_cups == 0:
-            possibility = 0
-            msg('disposable cups')
-        if possibility:
-            qty_water -= Lat_reqd_water
-            qty_milk -= Lat_reqd_milk
-            qty_beans -= Lat_reqd_beans
-            no_of_cups -= 1
-            money += Lat_cost
-            print('I have enough resources, making you a coffee!')
-        print()
-    elif inp == '3':
-        if qty_water < Cap_reqd_water:
-            msg('water')
-            possibility = 0
+            self.take()
+        elif inp == 'remaining':
             print()
-        elif qty_milk < Cap_reqd_milk:
-            msg('milk')
-            possibility = 0
-            print()
-        elif qty_beans < Cap_reqd_beans:
-            msg('coffee beans')
-            possibility = 0
-            print()
-        elif no_of_cups == 0:
-            possibility = 0
-            msg('disposable cups')
-        if possibility:
-            qty_water -= Cap_reqd_water
-            qty_milk -= Cap_reqd_milk
-            qty_beans -= Cap_reqd_beans
-            no_of_cups -= 1
-            money += Cap_cost
-            print('I have enough resources, making you a coffee!')
-        print()
-    elif inp == 'back':
-        main()
+            self.output()
+        elif inp == 'exit':
+            return inp
 
 
-def main():
-    inp = input('Write action (buy, fill, take, remaining, exit): ')
-    if inp == 'buy':
-        print()
-        buy()
-    elif inp == 'fill':
-        print()
-        fill()
-    elif inp == 'take':
-        print()
-        take()
-    elif inp == 'remaining':
-        print()
-        output()
-    elif inp == 'exit':
-        return inp
-
-
+cm = CoffeeMachine(400, 540, 120, 9, 550)
 while True:
-    out = main()
+    out = cm.main()
     if out == 'exit':
         break
